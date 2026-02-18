2f34a09d0ed3188cbc764ff2c070a1b9de9b8c00
Timon Gemberg-Wiesike <gembergwiesike@outlook.de>
2026-02-18 14:41:42 +0100

    tests und workflow hinzugefügt

---
+++ tests/test_fairsharer.py
@@ -0,0 +1,8 @@
+from src.fair_sharer import fair_sharer
+
+def test_fair_sharer():
+    result1 = fair_sharer([0, 1000, 800, 0], 1)
+    assert result1 == [100, 800, 900, 0]
+    
+    result2 = fair_sharer([0, 1000, 800, 0], 2)
+    assert result2 == [100, 890, 720, 90]
